8043393fd3f6e42a77a5352e9ae725536aa9ee9f
allen <allen.xie@lexisnexis.com>
2017-04-18 17:10:32 +0800

    proxy

--- SeleniumPhantomJSSample/SeleniumPhantomJSSample/Program.cs
+++ SeleniumPhantomJSSample/SeleniumPhantomJSSample/Program.cs
@@ -10,18 +10,24 @@ namespace SeleniumPhantomJSSample
         static void Main(string[] args)
         {
             var url = "https://passport.cnblogs.com/user/signin";
-           IWebDriver driver1 = new PhantomJSDriver(GetPhantomJSDriverService());
+           var  driver1 = new PhantomJSDriver(GetPhantomJSDriverService());
             driver1.Navigate().GoToUrl(url);
+           
             if (driver1.Title == "用户登录 - 博客园")
             {
                 driver1.FindElement(By.Id("input1")).SendKeys("xielongbao");
                 driver1.FindElement(By.Id("input2")).SendKeys("1234");
                 driver1.FindElement(By.Id("signin")).Click();
             }
+            driver1.GetScreenshot().SaveAsFile(@"C:\aa.png", ScreenshotImageFormat.Png);
+            var o = driver1.ExecuteScript("$('#signin').val('dsa')");
+        
             Console.WriteLine(driver1.PageSource);
-
+            driver1.Navigate().GoToUrl(url);
+            Console.WriteLine(driver1.PageSource);
             IWebDriver driver2 = new PhantomJSDriver(GetPhantomJSDriverService());
             driver2.Navigate().GoToUrl("https://home.cnblogs.com/");
+            
             Console.WriteLine(driver2.PageSource);
             Console.WriteLine(driver1.PageSource);
 
@@ -30,12 +36,13 @@ namespace SeleniumPhantomJSSample
 
         private static PhantomJSDriverService GetPhantomJSDriverService()
         {
-            PhantomJSDriverService pds = PhantomJSDriverService.CreateDefaultService();
-            //设置代理服务器地址
-            //pds.Proxy = $"{ip}:{port}";  
-            //设置代理服务器认证信息
-            //pds.ProxyAuthentication = GetProxyAuthorization();
-            return pds;
+            PhantomJSDriverService service = PhantomJSDriverService.CreateDefaultService();
+            //Proxy proxy = new Proxy();
+            //proxy.HttpProxy = string.Format("127.0.0.1:9999");
+      
+            //service.ProxyType = "http";
+            //service.Proxy = proxy.HttpProxy;
+            return service;
         }
     }
 }
